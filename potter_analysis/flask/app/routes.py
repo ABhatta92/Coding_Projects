6a6710883387b78656abdf59bd1f0c2c610c8992
ABhatta92 <abhiroop2792@gmail.com>
2024-07-14 18:55:04 +0530

    updated routes.py

--- potter_analysis/flask/app/routes.py
+++ potter_analysis/flask/app/routes.py
@@ -11,34 +11,32 @@ df = pd.read_csv(path_to_data, index_col=0)
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    selected_entity = request.form.get('Entities', 'All')
     selected_book = request.form.get('Book_Number', 'All')
+    selected_entity = request.form.get('Entities', 'All')
 
-    if (selected_entity == 'All') & (selected_book == 'All'):
-        df_filtered = df
-    elif (selected_book == 'All'):
-        df_filtered = df[df_filtered['Entities'] == selected_entity]
-    else:
-        df_filtered = df[(df_filtered['Entities'] == selected_entity) & (df_filtered['Book_Number'] == selected_book)]
-
+    df_filtered = df
+    if selected_book != 'All':
+        df_filtered = df_filtered[df_filtered['Book_Number'] == int(selected_book)]
+    if selected_entity != 'All':
+        df_filtered = df_filtered[df_filtered['Entities'] == selected_entity]
 
     df_aggregated = df_filtered.drop(['Sentiment', 'Subjectivity'], axis=1).groupby(['Book_Number', 'Page_Number', 'Entities']).agg({'Polarity': 'sum'}).reset_index()
 
     fig = px.line(df_aggregated,
-                     x='Page_Number',
-                     y='Polarity',
-                     color='Entities',
-                     facet_col='Book_Number',
-                     title='Sum of Sentiment by Book Number and Page Number',
-                     labels={'Page_Number': 'Page Number', 'Polarity': 'Sum of Sentiment', 'Entities': 'Entity Name'},
-                     template='plotly_white')
+                  x='Page_Number',
+                  y='Polarity',
+                  color='Entities',
+                  facet_col='Book_Number',
+                  title='Sum of Sentiment by Book Number and Page Number',
+                  labels={'Page_Number': 'Page Number', 'Polarity': 'Sum of Sentiment', 'Entities': 'Entity Type'},
+                  template='plotly_white')
 
     plot_html = pio.to_html(fig, full_html=False)
 
     entities = df['Entities'].unique()
     entities = sorted(entities.tolist() + ['All'])
-
+    
     books = df['Book_Number'].unique()
     books = sorted(books.tolist() + ['All'])
 
-    return render_template('index.html', plot_html=plot_html, entities=entities, selected_entity=selected_entity)
+    return render_template('index.html', plot_html=plot_html, entities=entities, books=books, selected_entity=selected_entity, selected_book=selected_book)
